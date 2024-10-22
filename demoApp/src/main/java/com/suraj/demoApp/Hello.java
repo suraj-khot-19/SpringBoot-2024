0a715c12a371d410fac91d963bfe772682d27b51
suraj-khot-19 <surajkhot19082001@gmail.com>
2024-10-22 13:34:41 +0530

    hello world

---
+++ demoApp/src/main/java/com/suraj/demoApp/Hello.java
@@ -0,0 +1,12 @@
+package com.suraj.demoApp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+    @RequestMapping("/")
+    public String greet(){
+        return "Hello World, This is from developer Suraj!";
+    }
+}
