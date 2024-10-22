08edda9bfe4771537b1eb3e7ebff9df305026d33
suraj-khot-19 <surajkhot19082001@gmail.com>
2024-10-22 18:53:35 +0530

    how bean create a unecessary objects means without using in pgm then also

--- SpringApp/src/main/java/org/suraj/Dev.java
+++ SpringApp/src/main/java/org/suraj/Dev.java
@@ -2,8 +2,10 @@ package org.suraj;
 
 import org.springframework.stereotype.Component;
 
-@Component
 public class Dev {
+    public Dev(){
+        System.out.println("Created a object of *Dev in container using config.xml file means in bean");
+    }
     public void build(){
         System.out.println("Building a project...");
     }
--- SpringApp/src/main/java/org/suraj/Laptop.java
+++ SpringApp/src/main/java/org/suraj/Laptop.java
@@ -1,6 +1,9 @@
 package org.suraj;
 
 public class Laptop {
+    public Laptop(){
+        System.out.println("Created a object of *Laptop in container using config.xml file means in bean");
+    }
     public void coding(){
         System.out.println("Coding is a life...");
     }
