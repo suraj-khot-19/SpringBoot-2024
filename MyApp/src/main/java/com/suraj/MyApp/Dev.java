e60335d2e5de89085403e31d96cca81dbe0fcd6d
suraj-khot-19 <surajkhot19082001@gmail.com>
2024-10-22 14:20:32 +0530

    dependency injection in spring

---
+++ MyApp/src/main/java/com/suraj/MyApp/Dev.java
@@ -0,0 +1,10 @@
+package com.suraj.MyApp;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dev {
+    public void build(){
+        System.out.println("Hello i am building a great project");
+    }
+}
---
+++ MyApp/src/main/java/com/suraj/MyApp/Laptop.java
@@ -0,0 +1,10 @@
+package com.suraj.MyApp;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+    public void laptop(){
+        System.out.println("With help of laptop. I love laptop");
+    }
+}
