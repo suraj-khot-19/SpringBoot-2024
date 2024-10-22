e056e6c678dca534ae4c2b87b5ca6fbce3102983
suraj-khot-19 <surajkhot19082001@gmail.com>
2024-10-22 18:41:52 +0530

    creating maven archetype (spring) and doing things by self how spring boot config

---
+++ SpringApp/src/main/java/org/suraj/App.java
@@ -0,0 +1,27 @@
+package org.suraj;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        /*
+        tradition way to call dev's method
+        Dev obj=new Dev();
+        obj.build();
+         */
+
+        //creating a container inside a jvm to store an objects like spring do
+        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+
+        //use bean here
+        // Dev obj = context.getBean(Dev.class);
+
+        /*
+        instead of writing Dev.class we can write "dev" cause of
+             <bean id="dev" class="org.suraj.Dev"> this here we declared dev as id just do type casting
+         */
+        Dev obj = (Dev) context.getBean("dev");
+        obj.build();
+    }
+}
---
+++ SpringApp/src/main/java/org/suraj/Dev.java
@@ -0,0 +1,10 @@
+package org.suraj;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dev {
+    public void build(){
+        System.out.println("Building a project...");
+    }
+}
