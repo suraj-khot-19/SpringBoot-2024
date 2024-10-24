cdede77fad8ddc2c5a9ad9127a0a35fd4e964fcf
suraj-khot-19 <surajkhot19082001@gmail.com>
2024-10-24 11:00:43 +0530

    seting up react for frontend

---
+++ WebApp/ecom_frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Nav from "./components/Nav";
+import Home from "./components/Home";
+
+function App() {
+  return (
+    <Router>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
